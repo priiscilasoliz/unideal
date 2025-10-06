e6c7540397a45094f9bea3ca0766909a8310d19b
Martin-Herrera1126 <26martinherrera@gmail.com>
2025-10-06 13:59:46 -0300

    .

--- unideal/unideal.sql
+++ unideal/unideal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-09-2025 a las 23:55:19
+-- Tiempo de generación: 06-10-2025 a las 18:57:49
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
